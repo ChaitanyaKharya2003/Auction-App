4fe3e4861daf1718b5a363c3ff8c77b76cd14d76
Chaitanya Kharya <kharyachaitanya@gmail.com>
2023-11-28 18:18:46 +0530

    Update CreatorApproval.tsx

--- src/components/Approval/CreatorApproval.tsx
+++ src/components/Approval/CreatorApproval.tsx
@@ -6,6 +6,11 @@ import { db, storage } from "../../firebaseConfig";
 import { getDownloadURL, ref } from "firebase/storage";
 import { error } from "console";
 import { Navigate } from "react-router-dom";
+import photos from "./playersPhotos23-24/3.jpg";
+import HH from "../../screens/Home/img/HH.png";
+import MB from "../../screens/Home/img/MB.png";
+import SS from "../../screens/Home/img/SS.png";
+import KK from "../../screens/Home/img/KK.png";
 
 const Approval = () => {
   const { playerId } = useParams();
@@ -109,51 +114,35 @@ const Approval = () => {
         <div className={styles.j}>
           <h2>Bid Placed</h2>{" "}
           {userData?.bidPoints !== 0 && <h2>Sold To {userData?.houseName}</h2>}
-          {userData?.bidPoints === 0 && <h2> {bid}</h2>}
+          {userData?.bidPoints === 0 && <h2 style={{fontSize:"2.5rem"}}> {bid}</h2>}
           <div className={styles.j}>
-          <button onClick={handleFiftyIncremental}>50</button>
-          <button onClick={handleHundredIncremental}>150</button>
-          <button onClick={handleTwoHunderdIncremental}>300</button>
-          <button onClick={handleRefresh}>Reset</button>
+            <button onClick={handleFiftyIncremental}>50</button>
+            <button onClick={handleHundredIncremental}>150</button>
+            <button onClick={handleTwoHunderdIncremental}>300</button>
+            <button onClick={handleRefresh}>Reset</button>
+          </div>
         </div>
-        </div>
-        <div className={styles.j}>
-          <h4>Photo</h4>{" "}
-          <img
-            src={`../playersPhoto23-24/${playerId}.jpg`}
-            alt="no photo found"
-            width={250}
-          />
-        </div>
-        <div className={styles.j}>
-          <h4>Name</h4> <p>{userData?.name}</p>
-        </div>
-        <div className={styles.j}>
-          <h4>Enrollment Number</h4> <p>{userData?.enrollment_num}</p>
-        </div>
-        <div className={styles.j}>
-          <h4>Course</h4> <p>{userData?.course}</p>
-        </div>
-        <div className={styles.j}>
-          <h4>Year</h4> <p>{userData?.year}</p>
-        </div>
-        <div className={styles.j}>
-          <h4>Score Out of 10</h4> <p>{userData?.score}</p>
-        </div>
-        <div className={styles.j}>
-          <h4>Sports Played</h4> <p>{userData?.sportsPlayed}</p>
-        </div>
-
-        <div className={styles.j}>
-          <button onClick={handleFiftyIncremental}>50</button>
-          <button onClick={handleHundredIncremental}>150</button>
-          <button onClick={handleTwoHunderdIncremental}>300</button>
-          <button onClick={handleRefresh}>Reset</button>
-        </div>
-      </div>
-
-      <div className={styles.section}>
-        <div className={styles.j}>
+        <div style={{ display: "flex" }}>
+          <div style={{}}>
+            <div className={styles.j}>
+              <h4>Name</h4> <p>{userData?.name}</p>
+            </div>
+            <div className={styles.j}>
+              <h4>Enrollment Number</h4> <p>{userData?.enrollment_num}</p>
+            </div>
+            <div className={styles.j}>
+              <h4>Course</h4> <p>{userData?.course}</p>
+            </div>
+            <div className={styles.j}>
+              <h4>Year</h4> <p>{userData?.year}</p>
+            </div>
+            <div className={styles.j}>
+              <h4>Score Out of 10</h4> <p>{userData?.score}</p>
+            </div>
+            <div className={styles.j}>
+              <h4>Sports Played</h4> <p>{userData?.sportsPlayed}</p>
+            </div>
+            <div className={styles.j}>
           <h4>Comments</h4>
           <input
             type="text"
@@ -165,42 +154,71 @@ const Approval = () => {
             }}
           ></input>
         </div>
+          </div>
+          <div className={styles.j}>
+            <img
+              src={`${photos}`}
+              alt="no photo found"
+              width={250}
+              style={{maxHeight:"350px"}}
+            />
+          </div>
+        </div>
+
+        {/* <div className={styles.j}>
+          <button onClick={handleFiftyIncremental}>50</button>
+          <button onClick={handleHundredIncremental}>150</button>
+          <button onClick={handleTwoHunderdIncremental}>300</button>
+          <button onClick={handleRefresh}>Reset</button>
+        </div> */}
+      </div>
+
+      <div className={styles.section}>
         {userData?.houseName === "" && (
           <div className={styles.j}>
-            <button
+            <button 
               onClick={() => {
                 handleHouse("Heroic Hearts");
               }}
+              style={{height:"100px"}}
             >
+              <img src={HH} alt="" height={"100px"} />
               Heroic Hearts
             </button>
             <button
               onClick={() => {
                 handleHouse("Super Spades");
               }}
+              style={{height:"100px"}}
             >
+              <img src={SS} alt="" height={"100px"} />
               Super Spades
             </button>
             <button
+              
               onClick={() => {
                 handleHouse("Mighty Boltz");
               }}
+              style={{height:"100px"}}
             >
+              <img src={MB} alt="" height={"100px"} />
               Mighty Boltz
             </button>
             <button
               onClick={() => {
                 handleHouse("Keen Kickers");
               }}
+              style={{height:"100px"}}
             >
+              <img src={KK} alt="" height={"100px"} />
               Keen Kickers
             </button>
           </div>
         )}
-        <div className={styles.j}>
+        {/* <div className={styles.j}>
           <button onClick={handlePrevious}>Previous</button>
           <button onClick={handleNext}>Next</button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
